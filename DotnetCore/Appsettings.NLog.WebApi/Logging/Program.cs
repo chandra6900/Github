b912d4ebbded7c6e320c81eb2fe9bbaeb39b7dcf
chandra6900 <chandra6900@gmail.com>
2022-03-13 12:47:00 +0530

    Added chages for configuring NLog from appsettings

--- DotnetCore/Appsettings.NLog.WebApi/Logging/Program.cs
+++ DotnetCore/Appsettings.NLog.WebApi/Logging/Program.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+using NLog.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +17,30 @@ namespace Logging
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+            ///
+            ///First way of configuring
+            ///
+
+            //var config = new ConfigurationBuilder()
+            //.SetBasePath(System.IO.Directory.GetCurrentDirectory())
+            //.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
+            //LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLog"));
+
+            //var logger = NLog.Web.NLogBuilder.ConfigureNLog(LogManager.Configuration).GetCurrentClassLogger();
+            //try
+            //{
+            //    logger.Debug("Init main");
+            //    CreateWebHostBuilder(args).Build().Run();
+            //}
+            //catch (Exception ex)
+            //{
+            //    logger.Error(ex, "Stopped program because of exception");
+            //}
+            //finally
+            //{
+            //    LogManager.Shutdown();
+            //}
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -21,6 +48,17 @@ namespace Logging
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .ConfigureLogging((hostBuilderContext, loggingBuilder)=>{
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                    ///
+                    ///Second way of configuring
+                    ///
+                    var section=hostBuilderContext.Configuration.GetSection("NLog");
+                    LogManager.Configuration = new NLogLoggingConfiguration(section);
+                    NLogBuilder.ConfigureNLog(LogManager.Configuration);
+                })
+                .UseNLog();
     }
 }
