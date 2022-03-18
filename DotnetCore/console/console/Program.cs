d715525dff34e08d434684961a36cbe3d38cbf53
chandra r <chandra6900@gmail.com>
2022-03-18 17:25:43 +0530

    modified names

--- DotnetCore/console/console/Program.cs
+++ DotnetCore/console/console/Program.cs
@@ -11,9 +11,9 @@ namespace console
     {
         static async Task Main(string[] args)
         {
-            string filePath = @"C:\Users\Office\Downloads\TestFile.txt";
+            string filePath = @"C:\Users\Office\Downloads\veggblcb_2.zip";
             //CreateFileRequest(filePath);
-            string url = @"https://localhost:44393/File/UploadFileBytesJson";
+            string url = @"https://localhost:5001/File/UploadFileBytesJson";
             string result= await UploadFileBytesJson(filePath, url);
             Console.WriteLine("Hello World!");
         }
@@ -27,18 +27,19 @@ namespace console
             fileJson.fileByteArray = Convert.ToBase64String(fileBytes);
             string fileJsonString = JsonSerializer.Serialize<JsonFileModel>(fileJson);
             File.WriteAllText(@"C:\Temp\FileRequest.txt", fileJsonString);
+
         }
         private static async Task<string> UploadFileBytesJson(string filePath, string url)
         {
             FileInfo fileInfo = new FileInfo(filePath);
             byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
-            JsonFileModel fileJson = new JsonFileModel();
-            fileJson.fileName = fileInfo.Name;
-            fileJson.fileByteArray = Convert.ToBase64String(fileBytes);
-            string fileJsonString = JsonSerializer.Serialize<JsonFileModel>(fileJson);
+            JsonFileModel fileJsonModel = new JsonFileModel();
+            fileJsonModel.fileName = fileInfo.Name;
+            fileJsonModel.fileByteArray = Convert.ToBase64String(fileBytes);
+            string fileJson = JsonSerializer.Serialize<JsonFileModel>(fileJsonModel);
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri(url);
-            HttpContent content = new StringContent(fileJsonString);
+            HttpContent content = new StringContent(fileJson);
             content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
             var result = await client.PostAsync(url, content);
             string resultContent = await result.Content.ReadAsStringAsync();
