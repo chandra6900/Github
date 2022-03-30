fbf860d06d27dc9576155a3bbc8eee8dea3a900e
chandra6900 <chandra6900@gmail.com>
2022-03-30 23:44:15 +0530

    committing custom size file

---
+++ DotnetCore/CustomSizeFile.Console/console/console/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteBytes(20, "mb");
+            Console.WriteLine("Hello World!");
+        }
+        static void WriteBytes(int size, string type)
+        {
+            string allowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string filePath = @"C:\\Temp\TestFile.txt";
+            int count = 0;
+            StringBuilder sb = new StringBuilder();
+
+            switch (type)
+            {
+                case ("Kb"):
+                    {
+                        count = 1024 * size;
+                        break;
+                    }
+                case ("mb"):
+                    {
+                        count = 1024 * 1024 * size;
+                        break;
+                    }
+                default:
+                    {
+                        count = size;
+                        break;
+                    }
+            }
+
+            for (int i = 0; i <= count; i++)
+            {
+                sb.Append("A");
+            }
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+            File.AppendAllText(filePath, sb.ToString());
+        }
+    }
+}
