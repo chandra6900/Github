d83e5d96eb0ff60762ec79ab2dc7dbc9e82a045a
chandra6900 <chandra6900@gmail.com>
2022-05-08 07:44:45 +0530

    Added projects need to  be coded

---
+++ DotnetCore/ExtensionMethods.OpenClosePrinciple.Console/ExtensionMethods.OpenClosePrinciple.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExtensionMethods.OpenClosePrinciple..Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DotnetCore/OverrideVirtual.LiskovSubstitutionPrinciple.Console/OverrideVirtual.LiskovSubstitutionPrinciple.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OverrideVirtual.LiskovSubstitutionPrinciple.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
